6a0f100f17efbf0fe9c001f8565a5352c207e356
Paul Winter <zimin.pavel.v@gmail.com>
2025-10-14 15:24:07 +0300

    работа в отсоединённом режиме

---
+++ Урок №2. Отсоединённый режим/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Урок__2_Отсоединённый_режим
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection connection = null;
+        private SqlDataAdapter da = null;
+        private DataSet dataSet = null;
+        private SqlCommandBuilder cmd = null;
+        private string connString = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            connString = ConfigurationManager.ConnectionStrings["AcademyConnectionString"].ConnectionString;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            da.Update(dataSet, "Groups");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection = new SqlConnection(connString);
+                dataSet = new DataSet();
+                string sqlCommand = textBox1.Text;
+                da = new SqlDataAdapter(sqlCommand, connection);
+                dataGridView1.DataSource = null;
+                cmd = new SqlCommandBuilder(da);
+                da.Fill(dataSet, "Groups");
+                dataGridView1.DataSource = dataSet.Tables["Groups"];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error!");
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Урок №2. Отсоединённый режим/Properties/AssemblyInfo.cs
+++ Урок №2. Отсоединённый режим/Properties/AssemblyInfo.cs
@@ -4,30 +4,30 @@ using System.Runtime.InteropServices;
 
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
-// связанные с этой сборкой.
-[assembly: AssemblyTitle("Урок №2 Подключение к БД")]
+// связанных со сборкой.
+[assembly: AssemblyTitle("Урок №2 Отсоединённый режим")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Урок №2 Подключение к БД")]
+[assembly: AssemblyProduct("Урок №2 Отсоединённый режим")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
 // для компонентов COM. Если необходимо обратиться к типу в этой сборке через
-// из модели COM задайте для атрибута ComVisible этого типа значение true.
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
 [assembly: ComVisible(false)]
 
-// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
-[assembly: Guid("fd6e37b6-de82-4437-a9df-6a8f95193c00")]
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b2cef4b7-a7d6-477e-93df-2aa9a745440a")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
 //      Основной номер версии
 //      Дополнительный номер версии
 //      Номер сборки
-//      Номер редакции
+//      Редакция
 //
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
