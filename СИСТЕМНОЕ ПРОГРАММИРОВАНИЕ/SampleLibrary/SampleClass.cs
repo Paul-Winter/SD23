5626d7388b67dfdb8c080d0f15273a312ade3786
Paul Winter <zimin.pavel.v@gmail.com>
2025-10-29 15:16:16 +0300

    добавлен проект использования домена приложения

---
+++ СИСТЕМНОЕ ПРОГРАММИРОВАНИЕ/SampleLibrary/SampleClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleLibrary
+{
+    public class SampleClass
+    {
+        public static void DoSome()
+        {
+            Console.WriteLine("Doing something");
+        }
+    }
+}
