251c49d3527ee2c8ac1e33f83b99dc547d58f97e
Paul Winter <zimin.pavel.v@gmail.com>
2025-11-05 14:36:05 +0300

    работа с потоками

--- СИСТЕМНОЕ ПРОГРАММИРОВАНИЕ/Урок №4. Многопоточность/Program.cs
+++ СИСТЕМНОЕ ПРОГРАММИРОВАНИЕ/Урок №4. Многопоточность/Program.cs
@@ -11,21 +11,47 @@ namespace Урок__4.Многопоточность
             Console.WriteLine("Hello in timer!");
         }
 
+        static void Method()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("\t\t\tHello in Thread!");
+                Thread.Sleep(1000);
+            }
+        }
 
         static void Main(string[] args)
         {
-            // 2. Создать объект делегата и связать с ним метод
-            TimerCallback callback = new TimerCallback(TimerMethod);
+            /* 2. Создать объект делегата и связать с ним метод
+            //TimerCallback callback = new TimerCallback(TimerMethod);
 
             // 3. Создать объект таймера и передать конструктор-делегат
-            Timer timer = new Timer(callback);
+            //Timer timer = new Timer(callback);
 
-            Console.WriteLine("Timer start!");
+            //Console.WriteLine("Timer start!");
 
             // 4. Указать интервал таймера        
-            timer.Change(10000, 2000);
+            //timer.Change(10000, 2000);
 
-            Console.ReadLine();
+            //Console.ReadLine();*/
+            // Создать объект делегата
+            ThreadStart threadStart = new ThreadStart(Method);
+
+            // Создать объект потока
+            Thread thread = new Thread(threadStart);
+
+            thread.IsBackground = true;
+
+            // Запуск потока
+            thread.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello in Main");
+                Thread.Sleep(300);
+            }
+
+            thread.Abort();
         }
     }
 }
