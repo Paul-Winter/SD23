8732fa1fbe57dee81507a55cb2843165984d9388
Paul Winter <zimin.pavel.v@gmail.com>
2025-11-11 12:53:37 +0300

    пример асинхронного вызова методов

---
+++ СИСТЕМНОЕ ПРОГРАММИРОВАНИЕ/Урок №6. Асинхронный вызов/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Урок__6.Асинхронный_вызов
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(@"../../Program.cs", FileMode.Open,
+                FileAccess.Read, FileShare.Read, 1024, FileOptions.Asynchronous);
+            byte[] info = new byte[256];
+            IAsyncResult result = fs.BeginRead(info, 0, info.Length, null, null);
+            int bytesRead = fs.EndRead(result);
+            Console.WriteLine($"Количество прочитанных байт = {bytesRead}");
+            Console.WriteLine(Encoding.UTF8.GetString(info));
+        }
+    }
+}
