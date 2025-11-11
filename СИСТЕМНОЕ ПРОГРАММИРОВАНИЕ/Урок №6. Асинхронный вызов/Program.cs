d6267a61ac1d67f0bc0bda6ae03c3ab22feadeb3
Paul Winter <zimin.pavel.v@gmail.com>
2025-11-11 13:31:23 +0300

    ещё пример асинхронного вызова методов

--- СИСТЕМНОЕ ПРОГРАММИРОВАНИЕ/Урок №6. Асинхронный вызов/Program.cs
+++ СИСТЕМНОЕ ПРОГРАММИРОВАНИЕ/Урок №6. Асинхронный вызов/Program.cs
@@ -8,13 +8,44 @@ namespace Урок__6.Асинхронный_вызов
     {
         static void Main(string[] args)
         {
-            FileStream fs = new FileStream(@"../../Program.cs", FileMode.Open,
-                FileAccess.Read, FileShare.Read, 1024, FileOptions.Asynchronous);
-            byte[] info = new byte[256];
-            IAsyncResult result = fs.BeginRead(info, 0, info.Length, null, null);
-            int bytesRead = fs.EndRead(result);
-            Console.WriteLine($"Количество прочитанных байт = {bytesRead}");
-            Console.WriteLine(Encoding.UTF8.GetString(info));
+            string[] files =
+            {
+                "../../Program.cs",
+                "../../Урок №6. Асинхронный вызов.csproj",
+                "../../Properties/AssemblyInfo.cs"
+            };
+            AsyncReader[] readers = new AsyncReader[files.Length];
+            for (int i = 0; i < files.Length; i++)
+            {
+                readers[i] = new AsyncReader(new FileStream(files[i], FileMode.Open,
+                    FileAccess.Read, FileShare.Read, 1024, FileOptions.Asynchronous), 1024);
+            }
+            foreach (AsyncReader reader in readers)
+            {
+                Console.WriteLine(reader.EndRead());
+            }
+        }
+    }
+
+    class AsyncReader
+    {
+        FileStream fs;
+        byte[] info;
+        IAsyncResult result;
+
+        public AsyncReader(FileStream s, int size)
+        {
+            fs = s;
+            info = new byte[size];
+            result = fs.BeginRead(info, 0, size, null, null);
+        }
+
+        public string EndRead()
+        {
+            int biteSize = fs.EndRead(result);
+            fs.Close();
+            Array.Resize(ref info, biteSize);
+            return $"File: {Encoding.UTF8.GetString(info)}\n\n{fs.Name}\n____________________";
         }
     }
 }
