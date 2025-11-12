56c8ee2b892d893d92f13eac5950d03daa36a675
Paul Winter <zimin.pavel.v@gmail.com>
2025-11-12 11:18:16 +0300

    проблемы синхронизации - гонка за ресурсом

---
+++ СИСТЕМНОЕ ПРОГРАММИРОВАНИЕ/Урок №6. Синхронизация/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Урок__6.Синхронизация
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[5];
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(delegate ()
+                {
+                    for (int j = 0; j < 1000000; j++)
+                    {
+                        ++Counter.count;
+                    }
+                });
+                threads[i].Start();
+            }
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Join();
+            }
+            Console.WriteLine($"counter = {Counter.count}");
+        }
+    }
+
+    class Counter
+    {
+        public static int count;
+    }
+}
