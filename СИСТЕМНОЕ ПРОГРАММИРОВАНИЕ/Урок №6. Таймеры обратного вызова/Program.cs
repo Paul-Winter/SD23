bbbcbcbcebdaa4368289d971fac64a5582a9deba
Paul Winter <zimin.pavel.v@gmail.com>
2025-11-12 11:03:39 +0300

    таймеры обратного вызова

---
+++ СИСТЕМНОЕ ПРОГРАММИРОВАНИЕ/Урок №6. Таймеры обратного вызова/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace Урок__6.Таймеры_обратного_вызова
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer(TimerMethod, null, 0, 1000);
+            Console.WriteLine($"Основной поток №{Thread.CurrentThread.ManagedThreadId} продолжается");
+            Thread.Sleep(5000);
+            timer.Dispose();
+        }
+        static void TimerMethod(Object obj)
+        {
+            Console.WriteLine($"Поток №{Thread.CurrentThread.ManagedThreadId} : {DateTime.Now}");
+        }
+    }
+}
