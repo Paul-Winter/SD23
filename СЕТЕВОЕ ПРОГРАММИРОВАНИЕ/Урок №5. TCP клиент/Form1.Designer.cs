70efdce4f5ed0f7ea3996689c3ee9507080d1f2e
Paul Winter <zimin.pavel.v@gmail.com>
2025-11-25 15:25:19 +0300

    some refactoring

--- СЕТЕВОЕ ПРОГРАММИРОВАНИЕ/Урок №5. TCP клиент/Form1.Designer.cs
+++ СЕТЕВОЕ ПРОГРАММИРОВАНИЕ/Урок №5. TCP клиент/Form1.Designer.cs
@@ -109,7 +109,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "CLIENT";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.ResumeLayout(false);
             this.PerformLayout();
--- СЕТЕВОЕ ПРОГРАММИРОВАНИЕ/Урок №5. TCP сервер/Form1.Designer.cs
+++ СЕТЕВОЕ ПРОГРАММИРОВАНИЕ/Урок №5. TCP сервер/Form1.Designer.cs
@@ -98,7 +98,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SERVER";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.ResumeLayout(false);
             this.PerformLayout();
