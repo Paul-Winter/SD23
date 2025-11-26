10f46aefb1c03117c3e1e206f495a77ef746cda0
Paul Winter <zimin.pavel.v@gmail.com>
2025-11-26 11:34:39 +0300

    udp клиент

---
+++ СЕТЕВОЕ ПРОГРАММИРОВАНИЕ/Урок №7. UDP клиент/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Урок__7.UDP_клиент
+{
+    internal class Program
+    {
+        static int remotePort;
+        static int localPort;
+        static IPAddress remoteAddress;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.SetWindowSize(40, 20);
+                Console.Title = "Чатик";
+                Console.Write("введите удалённый IP-адрес: ");
+                remoteAddress = IPAddress.Parse(Console.ReadLine());
+                Console.Write("введите удалённый порт: ");
+                remotePort = Convert.ToInt32(Console.ReadLine());
+                Console.Write("введите локальный порт: ");
+                localPort = Convert.ToInt32(Console.ReadLine());
+                Thread thread = new Thread(new ThreadStart(ReceiveFunc));
+                thread.IsBackground = true;
+                thread.Start();
+                Console.ForegroundColor = ConsoleColor.Red;
+                while (true)
+                {
+                    SendFunc(Console.ReadLine());
+                }
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine($"Неверный формат: {fe.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        static void ReceiveFunc()
+        {
+            while (true)
+            {
+                UdpClient client = null;
+                try
+                {
+                    client = new UdpClient(localPort);
+                    IPEndPoint endPoint = null;
+                    byte[] responce = client.Receive(ref endPoint);
+                    string result = Encoding.Unicode.GetString(responce);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(result);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine($"Ошибка сокета: {se.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+                }
+                finally
+                {
+                    client.Close();
+                }
+            }
+        }
+
+        static void SendFunc(string message)
+        {
+            UdpClient client = new UdpClient();
+            IPEndPoint endPoint = new IPEndPoint(remoteAddress, remotePort);
+            try
+            {
+                byte[] buffer = Encoding.Unicode.GetBytes(message);
+                client.Send(buffer, buffer.Length, endPoint);
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine($"Ошибка сокета: {se.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+    }
+}
---
+++ СЕТЕВОЕ ПРОГРАММИРОВАНИЕ/Урок №7. UDP клиент/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Урок №7. UDP клиент")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Урок №7. UDP клиент")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("3fb68a1f-7150-43a5-97fc-3e40f7e9f6dc")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
