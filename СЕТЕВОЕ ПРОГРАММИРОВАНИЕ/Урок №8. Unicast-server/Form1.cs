27ab44432d321f12036c66fc80507532bf03fafe
Paul Winter <zimin.pavel.v@gmail.com>
2025-12-04 11:36:43 +0300

    multicast client and server

---
+++ СЕТЕВОЕ ПРОГРАММИРОВАНИЕ/Урок №8. Unicast-server/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Урок__8.Unicast_server
+{
+    public partial class Form1 : Form
+    {
+        static string message = "Hello there!!!";
+        static int interval = 1000;
+
+        static void MulticastSend()
+        {
+            while (true)
+            {
+                Thread.Sleep(interval);
+                Socket socket = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Dgram, ProtocolType.Udp);
+                socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 2);
+                IPAddress dest = IPAddress.Parse("224.5.5.5");
+                socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(dest));
+                IPEndPoint endPoint = new IPEndPoint(dest, 4567);
+                socket.Connect(endPoint);
+                socket.Send(Encoding.Default.GetBytes(message));
+                socket.Close();
+            }
+        }
+
+        Thread sender = new Thread(new ThreadStart(MulticastSend));
+
+        public Form1()
+        {
+            InitializeComponent();
+            sender.IsBackground = true;
+            sender.Start();
+        }
+
+        private void textBox1_TextChanged(object sender, System.EventArgs e)
+        {
+            message = textBox1.Text;
+        }
+    }
+}
---
+++ СЕТЕВОЕ ПРОГРАММИРОВАНИЕ/Урок №8. Unicast/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Урок__8.Unicast
+{
+    public partial class Form1 : Form
+    {
+        delegate void AppendText(string text);
+        void AppendTextProc(string text)
+        {
+            textBox1.Text = text;
+        }
+
+        void Listener()
+        {
+            while (true)
+            {
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 4567);
+                socket.Bind(endPoint);
+                IPAddress ip = IPAddress.Parse("224.5.5.5");
+                socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership,
+                    new MulticastOption(ip, IPAddress.Any));
+                byte[] buffer = new byte[1024];
+                socket.Receive(buffer);
+                this.Invoke(new AppendText(AppendTextProc), Encoding.Default.GetString(buffer));
+                socket.Close();
+            }
+        }
+
+        Thread listen;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listen = new Thread(new ThreadStart(Listener));
+            listen.IsBackground = true;
+            listen.Start();
+        }
+    }
+}
