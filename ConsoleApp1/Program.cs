e52f6b60da9316ac775cc8e28abf38a2c607e4ba
Didar1505 <didarkamiljanow15@gmail.com>
2025-12-16 14:04:18 +0300

    практика лушникова

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+        // Метод - это подпрограмма, принадлежащая классу 
+        // Функция - это подпрограмма, принимающая и возвращающая значения
+        // Процедура - это функция не возвращающая значений 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, Мир. Из класса 1");
+        }
+
+    static void M1() { }
+    }
+
+
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Привет, Мир!");
+        //}
+
+        //Console.WriteLine("Привет, Мир!");
+    }
+}
