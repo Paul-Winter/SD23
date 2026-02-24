773964a60a978f6c76bbcc2b863bef9779e38033
Paul Winter <zimin.pavel.v@gmail.com>
2026-02-24 11:02:27 +0300

    работа с событиями и стилями

--- HTML5/js second project/script.js
+++ HTML5/js second project/script.js
@@ -1,4 +1,8 @@
-function moveDIV(e) {
-    DIV.style.left = e.pageX+'px';
-    DIV.style.top = e.pageY+'px';
+function colorDIV(e) {
+    DIV.style.left = e.pageX + "px";
+    DIV.style.top = e.pageY + "px";
+    DIV.style.backgroundColor="rgb(" +
+        Math.round(255*Math.random()) + "," +
+        Math.round(255*Math.random()) + "," +
+        Math.round(255*Math.random()) + ")";
 }
