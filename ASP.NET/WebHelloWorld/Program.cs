ee7489a29caae507a99ed7df089b1c2f95a4328c
Paul Winter <zimin.pavel.v@gmail.com>
2026-09-04 18:02:05 +0300

    отправка json

--- ASP.NET/WebHelloWorld/Program.cs
+++ ASP.NET/WebHelloWorld/Program.cs
@@ -4,6 +4,7 @@ namespace WebHelloWorld
 {
     public class Program
     {
+        public record User(string login, string password);
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
@@ -12,21 +13,10 @@ namespace WebHelloWorld
             app.Run(async (context) =>
             {
                 var response = context.Response;
+                //response.ContentType = "text/html; charset=utf-8";
+                User userdata = new User("John Doe", "qwerty111");
 
-                response.ContentType = "text/html; charset=utf-8";
-
-                if (context.Request.Path == "/postdata")
-                {
-                    var form = context.Request.Form;
-                    string login = form["login"];
-                    string password = form["pass"];
-                    await context.Response.WriteAsync($"<h3>Login: {login}</h3>" +
-                        $"<h3>Password: {password}</h3>");                
-                }
-                else
-                {
-                    await context.Response.SendFileAsync("html/index.html");
-                }
+                await response.WriteAsJsonAsync(userdata);
             });
             app.Run();
         }
