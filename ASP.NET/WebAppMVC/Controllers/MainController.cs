85a134f01fab57d8dc8dda7b2fe4f1d687aba8f2
Paul Winter <zimin.pavel.v@gmail.com>
2026-09-21 14:51:55 +0300

    типы запросов для методов-действий

--- ASP.NET/WebAppMVC/Controllers/MainController.cs
+++ ASP.NET/WebAppMVC/Controllers/MainController.cs
@@ -5,16 +5,19 @@ namespace WebAppMVC.Controllers
     //[NonController] - отключаем контроллер
     public class MainController : Controller
     {
+        [HttpGet]
         public string Index()
         {
             return "This is Index action!";
         }
         //[ActionName("Greeting")] - меняем имя действия
+        [HttpPost]
         public string Hello()
         {
             return "Hello, World!";
         }
-        [NonAction] // - отключаем действие (не все методы - действия)
+        //[NonAction] // - отключаем действие (не все методы - действия)
+        [HttpDelete]
         public string Greeting()
         {
             return "Hello, User!";
